f8babd6d86954cd57c58f00d7ddb63fa1a1925a7
Guilherme <guilhermeluiscp@gmail.com>
2020-05-05 23:12:11 -0300

    Finalização home html e criacao logica de salas

--- pages/pages.js
+++ pages/pages.js
@@ -7,14 +7,100 @@ document.onreadystatechange = function(e) {
     }
 };
 onload = () => {
-    criarSala.onsubmit = (evento) => {
-        alert('click criar');
-
-    };
+    var app = new Vue({
+        el: '#app',
+        data: {
+            roomsCriadas: (JSON.parse(localStorage.getItem('rooms')).filter((room) => {
+                return (room.criadoPor == JSON.parse(sessionStorage.getItem('userAtual')).id);
+            })),
+            roomsParticipadas: JSON.parse(localStorage.getItem('rooms')).filter((room) => {
+                return (room.invites.find((user) => {
+                    return (user.id == JSON.parse(sessionStorage.getItem('userAtual')).id);
+                }));
+            }),
+            rooms: JSON.parse(localStorage.getItem('rooms')),
+            users: JSON.parse(localStorage.getItem('users')),
+        },
+        methods: {
+            concatenateUsersInvite(invites) {
+                let names = "Você";
+                invites.forEach((invite, i) => {
+                    names += ", ";
+                    names += invite.nome;
+                });
+                return names;
+            },
+            findUserById(id) {
+                return this.users.find((user) => {
+                    return (user.id == id);
+                });
+            }
+        }
+    });
 
     entrarSala.onsubmit = (evento) => {
         alert('click entrar');
+        if (nomeSalaEntrar.value != "" && senhaSalaEntrar.value != "") {
+            let rooms = JSON.parse(localStorage.getItem('rooms'));
+            let found = rooms.find((room) => {
+                return (room.nome == document.getElementById('nomeSalaEntrar').value);
+            });
+            if (found == undefined) {
+                alert('Essa sala não existe, deseja criá-la?');
+                evento.preventDefault();
+            } else {
+                alert('achou');
 
+                if (found.senha == document.getElementById('senhaSalaEntrar').value) {
+                    let userAtual = JSON.parse(sessionStorage.getItem('userAtual'));
+                    let indexFound = rooms.findIndex((room) => {
+                        return (room.nome == document.getElementById('nomeSalaEntrar').value);
+                    });
+                    if (found.criadoPor != userAtual.id) {
+                        rooms[indexFound].invites.push(userAtual);
+                        localStorage.setItem('rooms', JSON.stringify(rooms));
+                    }
+                } else {
+                    alert('A senha inserida é inválida!');
+                    evento.preventDefault();
+                }
+                // sessionStorage.setItem('userAtual', JSON.stringify(found));
+            }
+        }
+    };
+
+    criarSala.onsubmit = (evento) => {
+        // alert('click criar');
+        if (nomeSalaCriar.value != "" && senhaSalaCriar.value != "") {
+            let rooms = JSON.parse(localStorage.getItem('rooms'));
+            let found = rooms.find((room) => {
+                return (room.nome == document.getElementById('nomeSalaCriar').value);
+            });
+            let userAtual = JSON.parse(sessionStorage.getItem('userAtual'));
+            if (found == undefined) {
+                let dataAgora = new Date();
+                let newRoom = {
+                    id: rooms.length + 1,
+                    nome: nomeSalaCriar.value,
+                    senha: senhaSalaCriar.value,
+                    criadoPor: userAtual.id,
+                    dataCriado: dataAgora.getDate() + "/" +
+                        (dataAgora.getMonth() + 1) + "/" +
+                        dataAgora.getFullYear() + " " +
+                        dataAgora.getHours() + ":" +
+                        dataAgora.getMinutes() + ":" +
+                        dataAgora.getSeconds(),
+                    invites: []
+                };
+                rooms.push(newRoom);
+                localStorage.setItem('rooms', JSON.stringify(rooms));
+            } else {
+                evento.preventDefault();
+                alert('Já existe uma sala com esse nome, tente novamente!');
+            }
+        } else {
+            alert("Digite as credenciais para entrar na sala!");
+        }
     };
 
     sair.onclick = (evento) => {
